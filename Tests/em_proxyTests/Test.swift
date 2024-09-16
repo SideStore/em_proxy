bdcf12c415f896e5e9e0e91e2ceab970f8ef9b27
Joe Mattiello <mail@joemattiello.com>
2024-09-16 10:29:04 -0400

    Add unit test example
    Signed-off-by: Joe Mattiello <mail@joemattiello.com>

--- Tests/em_proxyTests/Test.swift
+++ Tests/em_proxyTests/Test.swift
@@ -12,7 +12,20 @@ import Testing
 struct Test {
 
     @Test func test_em_proxy() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
 
+        var result: EMPError? = em_proxy.start_emotional_damage("127.0.0.1")
+        #expect(result == nil)
+        
+        result = em_proxy.start_emotional_damage("127.0.0.1")
+        
+        #expect(result! == .AlreadyRunning)
+        
+        em_proxy.stop_emotional_damage()
+    }
+    
+    @Test func test_em_proxy_test() async throws {
+        var result: EMPError? = em_proxy.test_emotional_damage(10)
+        
+        #expect(result == nil)
+    }
 }
